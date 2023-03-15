74f7129d5ab6de7ad63b26d57cd844debffe4c46
dionpouw <aldion.sumampouw@gmail.com>
2023-03-15 11:04:18 +0800

    custom edit text

---
+++ app/src/main/java/com/jeflette/mystory/component/PasswordEditText.kt
@@ -0,0 +1,46 @@
+package com.jeflette.mystory.component
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import com.jeflette.mystory.R
+
+class PasswordEditText : AppCompatEditText {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context, attrs, defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.length < 6) {
+                    this@PasswordEditText.error = R.string.password_less_than_6.toString()
+                } else {
+                    this@PasswordEditText.error = null
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+}
