8063dfa125eb9b9ea42b430c2b0f5a486e6b9766
dionpouw <aldion.sumampouw@gmail.com>
2023-03-24 22:47:10 +0800

    Story Adapter

--- app/src/main/java/com/jeflette/mystory/presentation/adapter/StoryAdapter.kt
+++ app/src/main/java/com/jeflette/mystory/presentation/adapter/StoryAdapter.kt
@@ -2,17 +2,27 @@ package com.jeflette.mystory.presentation.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.jeflette.mystory.R
 import com.jeflette.mystory.data.local.entity.Story
 import com.jeflette.mystory.databinding.ItemStoryBinding
+import com.jeflette.mystory.presentation.homeFragment.HomeFragmentDirections
+import com.jeflette.mystory.util.StoryDiffCallback
+import com.jeflette.mystory.util.convertDatetime
 
 class StoryAdapter : RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
 
     var storyList = ArrayList<Story>()
 
-    fun setData(storyList: ArrayList<Story>) {
-        this.storyList = storyList
+    fun setData(newStoryList: ArrayList<Story>) {
+        val diffUtil = StoryDiffCallback(storyList, newStoryList)
+        val diffResult = DiffUtil.calculateDiff(diffUtil)
+        storyList.clear()
+        storyList.addAll(newStoryList)
+        diffResult.dispatchUpdatesTo(this)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder =
@@ -22,10 +32,19 @@ class StoryAdapter : RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
             )
         )
 
-
     override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
         holder.bind(storyList[position])
         holder.itemView.setOnClickListener {
+            val actionHomeFragmentToDetailFragment =
+                HomeFragmentDirections.actionHomeFragmentToDetailFragment(storyList[position])
+
+            findNavController(it).currentDestination?.id?.let { id ->
+                when (id) {
+                    R.id.homeFragment -> findNavController(it).navigate(
+                        actionHomeFragmentToDetailFragment
+                    )
+                }
+            }
         }
     }
 
@@ -35,10 +54,10 @@ class StoryAdapter : RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
         RecyclerView.ViewHolder(itemBinding.root) {
         fun bind(story: Story) {
             itemBinding.apply {
-                Glide.with(itemView.context).load(story.photoUrl).into(itemStoryImg)
-                itemStoryName.text = story.name
-                itemStoryDesc.text = story.description
-                itemStoryDate.text = story.createdAt
+                Glide.with(itemView.context).load(story.photoUrl).into(ivItemPhoto)
+                tvItemName.text = story.name
+                tvItemDesc.text = story.description
+                tvItemDate.text = convertDatetime(story.createdAt.toString())
             }
         }
     }
