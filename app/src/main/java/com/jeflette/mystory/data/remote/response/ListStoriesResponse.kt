ecea8d3cb41e679314a654c39af8bfa66df55f8d
dionpouw <aldion.sumampouw@gmail.com>
2023-03-24 22:49:18 +0800

    Response for home fragment

--- app/src/main/java/com/jeflette/mystory/data/remote/response/ListStoriesResponse.kt
+++ app/src/main/java/com/jeflette/mystory/data/remote/response/ListStoriesResponse.kt
@@ -1,39 +1,13 @@
 package com.jeflette.mystory.data.remote.response
 
 import com.google.gson.annotations.SerializedName
+import com.jeflette.mystory.data.local.entity.Story
 
 data class ListStoriesResponse(
 
-	@field:SerializedName("listStory")
-	val listStory: List<ListStoryItem>? = null,
+    @field:SerializedName("listStory") val listStory: List<Story>? = null,
 
-	@field:SerializedName("error")
-	val error: Boolean? = null,
+    @field:SerializedName("error") val error: Boolean? = null,
 
-	@field:SerializedName("message")
-	val message: String? = null
-)
-
-data class ListStoryItem(
-
-	@field:SerializedName("photoUrl")
-	val photoUrl: String? = null,
-
-	@field:SerializedName("createdAt")
-	val createdAt: String? = null,
-
-	@field:SerializedName("name")
-	val name: String? = null,
-
-	@field:SerializedName("description")
-	val description: String? = null,
-
-	@field:SerializedName("lon")
-	val lon: Any? = null,
-
-	@field:SerializedName("id")
-	val id: String? = null,
-
-	@field:SerializedName("lat")
-	val lat: Any? = null
+    @field:SerializedName("message") val message: String? = null
 )
