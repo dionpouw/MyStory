ca5b5a3ca786a74331a6769a0b3945b418f3b744
dionpouw <aldion.sumampouw@gmail.com>
2023-03-24 22:47:39 +0800

    Detail Fragment + Viewmodel

--- app/src/main/java/com/jeflette/mystory/presentation/detailFragment/DetailFragment.kt
+++ app/src/main/java/com/jeflette/mystory/presentation/detailFragment/DetailFragment.kt
@@ -5,16 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.jeflette.mystory.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
+    private val args: DetailFragmentArgs by navArgs()
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding?.root
@@ -22,7 +23,11 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = null
+        binding?.apply {
+            Glide.with(this@DetailFragment).load(args.story.photoUrl).into(ivDetailPhoto)
+            tvDetailName.text = args.story.name
+            tvDetailDescription.text = args.story.description
+        }
     }
 
     override fun onDetach() {
