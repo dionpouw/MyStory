522ee4d63b43ec25b28648c768f9c98ab98e969a
dionpouw <aldion.sumampouw@gmail.com>
2023-03-24 22:47:22 +0800

    Fragment + ViewModel Home

--- app/src/main/java/com/jeflette/mystory/presentation/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/jeflette/mystory/presentation/homeFragment/HomeFragment.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jeflette.mystory.databinding.FragmentHomeBinding
+import com.jeflette.mystory.presentation.adapter.StoryAdapter
+import com.jeflette.mystory.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding
+    private val viewModel: HomeViewModel by viewModels()
+
+    init {
+        getTokenAndStories()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -23,6 +39,87 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val storiesAdapter = StoryAdapter()
+
+        binding?.apply {
+            actionLogout.setOnClickListener {
+                viewModel.clearLogin()
+                observeToken()
+            }
+            fabAddStory.setOnClickListener {
+                findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToAddStoryFragment())
+            }
+            rvStory.apply {
+                adapter = storiesAdapter
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+            }
+        }
+        populateList(storiesAdapter)
+    }
+
+    private fun observeToken() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.tokenResult.collectLatest {
+                    if (it == "") {
+                        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToLoginFragment())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun populateList(storiesAdapter: StoryAdapter) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.storiesResult.collectLatest {
+                    when (it) {
+                        is Resource.Success -> {
+                            binding?.apply {
+                                progressBar.visibility = View.GONE
+                                rvStory.visibility = View.VISIBLE
+                                errorMessage.visibility = View.GONE
+                            }
+                            it.data?.listStory?.let { listStory ->
+                                storiesAdapter.setData(
+                                    listStory as ArrayList
+                                )
+                            }
+                        }
+                        is Resource.Error -> {
+                            binding?.apply {
+                                progressBar.visibility = View.GONE
+                                rvStory.visibility = View.GONE
+                                errorMessage.apply {
+                                    visibility = View.VISIBLE
+                                    text = it.error?.message
+                                }
+                            }
+                            Toast.makeText(context, it.error?.message, Toast.LENGTH_SHORT).show()
+                        }
+                        is Resource.Loading -> {
+                            binding?.apply {
+                                progressBar.visibility = View.VISIBLE
+                                rvStory.visibility = View.GONE
+                                errorMessage.visibility = View.GONE
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getTokenAndStories() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.tokenResult.collectLatest { token ->
+                    viewModel.getStories(token)
+                }
+            }
+        }
     }
 
     override fun onDetach() {
--- app/src/main/java/com/jeflette/mystory/presentation/homeFragment/HomeViewModel.kt
+++ app/src/main/java/com/jeflette/mystory/presentation/homeFragment/HomeViewModel.kt
@@ -1,6 +1,53 @@
 package com.jeflette.mystory.presentation.homeFragment
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jeflette.mystory.data.MyStoryRepository
+import com.jeflette.mystory.data.remote.response.ListStoriesResponse
+import com.jeflette.mystory.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: MyStoryRepository
+) : ViewModel() {
+
+    private var _storiesResult: MutableStateFlow<Resource<ListStoriesResponse?>> =
+        MutableStateFlow(Resource.Loading())
+    val storiesResult: StateFlow<Resource<ListStoriesResponse?>> = _storiesResult
+
+    private var _tokenResult: MutableStateFlow<String> = MutableStateFlow("")
+    val tokenResult: StateFlow<String> = _tokenResult
+
+    init {
+        readToken()
+    }
+
+    fun getStories(value: String) {
+        viewModelScope.launch {
+            repository.getStories(token = "Bearer $value").collectLatest { it ->
+                _storiesResult.value = it
+            }
+        }
+    }
+
+    private fun readToken() {
+        viewModelScope.launch {
+            repository.readToken().collect() { token ->
+                _tokenResult.value = token
+            }
+        }
+    }
+
+    fun clearLogin(){
+        viewModelScope.launch {
+            repository.clearLogin()
+        }
+    }
 }
