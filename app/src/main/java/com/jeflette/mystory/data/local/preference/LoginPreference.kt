fa3e4a7a35920ec01fc47b5f4cd85414596c4697
dionpouw <aldion.sumampouw@gmail.com>
2023-03-24 22:48:34 +0800

    Login Fragment

--- app/src/main/java/com/jeflette/mystory/data/local/preference/LoginPreference.kt
+++ app/src/main/java/com/jeflette/mystory/data/local/preference/LoginPreference.kt
@@ -1,4 +1,46 @@
 package com.jeflette.mystory.data.local.preference
 
-class LoginPreference {
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class LoginPreference @Inject constructor(
+    private val dataStore: DataStore<Preferences>
+) {
+    private val isLogin= booleanPreferencesKey("is_login")
+    private val token = stringPreferencesKey("token")
+    private val username = stringPreferencesKey("username")
+
+    fun readToken(): Flow<String> {
+        return dataStore.data.map { preferences ->
+            preferences[token] ?: ""
+        }
+    }
+
+    fun readUsername(): Flow<String> {
+        return dataStore.data.map { preferences ->
+            preferences[username] ?: ""
+        }
+    }
+
+    suspend fun saveLogin(userToken: String,userUsername:String) {
+        dataStore.edit { preferences ->
+            preferences[token] = userToken
+            preferences[isLogin] = true
+            preferences[username] = userUsername
+        }
+    }
+
+    suspend fun clearLogin() {
+        dataStore.edit { preferences ->
+            preferences[isLogin] = false
+            preferences[token] = ""
+            preferences[username] = ""
+        }
+    }
 }
--- app/src/main/java/com/jeflette/mystory/presentation/loginFragment/LoginFragment.kt
+++ app/src/main/java/com/jeflette/mystory/presentation/loginFragment/LoginFragment.kt
@@ -1,23 +1,32 @@
 package com.jeflette.mystory.presentation.loginFragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.jeflette.mystory.databinding.FragmentLoginBinding
+import com.jeflette.mystory.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
 
-
+    private val viewModel: LoginViewModel by viewModels()
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding?.root
@@ -25,6 +34,83 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        checkPassword(binding)
+        observeToken()
+
+        binding?.loginButton?.setOnClickListener {
+            val email = binding?.edLoginEmail?.text.toString()
+            val password = binding?.edLoginPassword?.text.toString()
+            when {
+                email.isEmpty() -> {
+                    binding?.emailEditTextLayout?.error = "Masukkan email"
+                }
+                password.isEmpty() -> {
+                    binding?.passwordEditTextLayout?.error = "Masukkan password"
+                }
+                else -> {
+                    viewModel.submitLogin(
+                        binding?.edLoginEmail?.text.toString(),
+                        binding?.edLoginPassword?.text.toString()
+                    )
+                }
+            }
+            observeLogin()
+            binding?.registerButton?.setOnClickListener {
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
+            }
+        }
+    }
+
+    private fun checkPassword(binding: FragmentLoginBinding?) {
+        binding?.edLoginPassword?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                binding.loginButton.isEnabled = (s.length >= 6)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+
+    private fun observeToken() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.tokenResult.collect() {
+                    if (it.isNotEmpty()) {
+                        findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun observeLogin() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.loginResult.collect() { response ->
+                    when (response) {
+                        is Resource.Success -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            Toast.makeText(
+                                requireContext(), "Login Success", Toast.LENGTH_SHORT
+                            ).show()
+                            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
+                        }
+                        is Resource.Error -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            Toast.makeText(
+                                requireContext(), response.error?.message, Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        is Resource.Loading -> {
+                            binding?.progressBar?.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onDetach() {
