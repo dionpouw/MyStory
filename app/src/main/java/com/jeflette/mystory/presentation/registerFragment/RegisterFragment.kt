4dc5c2f4f9c41ff304d625260ded1ece120c2cd6
dionpouw <aldion.sumampouw@gmail.com>
2023-03-24 22:47:03 +0800

    Fragment register

--- app/src/main/java/com/jeflette/mystory/presentation/registerFragment/RegisterFragment.kt
+++ app/src/main/java/com/jeflette/mystory/presentation/registerFragment/RegisterFragment.kt
@@ -4,26 +4,66 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.jeflette.mystory.databinding.FragmentAddStoryBinding
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.jeflette.mystory.databinding.FragmentRegisterBinding
+import com.jeflette.mystory.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RegisterFragment : Fragment() {
 
-    private var _binding: FragmentAddStoryBinding? = null
+    private val viewModel: RegisterViewModel by viewModels()
+    private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentAddStoryBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding?.registerButton?.setOnClickListener {
+            viewModel.submitRegister(
+                binding?.edRegisterName?.text.toString(),
+                binding?.edRegisterEmail?.text.toString(),
+                binding?.edRegisterPassword?.text.toString()
+            )
+            observeRegister()
+        }
+    }
+
+    private fun observeRegister() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.registerResult.collect() { response ->
+                    when (response) {
+                        is Resource.Success -> {
+                            Toast.makeText(requireContext(), "Register Success", Toast.LENGTH_SHORT)
+                                .show()
+                            findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
+                        }
+                        is Resource.Error -> {
+                            Toast.makeText(
+                                requireContext(), response.error?.message, Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        is Resource.Loading -> {
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onDetach() {
--- app/src/main/java/com/jeflette/mystory/presentation/registerFragment/RegisterViewModel.kt
+++ app/src/main/java/com/jeflette/mystory/presentation/registerFragment/RegisterViewModel.kt
@@ -1,6 +1,31 @@
 package com.jeflette.mystory.presentation.registerFragment
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jeflette.mystory.data.MyStoryRepository
+import com.jeflette.mystory.data.remote.response.CommonResponse
+import com.jeflette.mystory.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RegisterViewModel : ViewModel() {
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    private val repository: MyStoryRepository
+) : ViewModel() {
+
+    private var _registerResult: MutableStateFlow<Resource<CommonResponse?>> =
+        MutableStateFlow(Resource.Loading())
+    val registerResult: StateFlow<Resource<CommonResponse?>> = _registerResult
+
+    fun submitRegister(name: String, email: String, password: String) {
+        viewModelScope.launch {
+            repository.submitRegister(name, email, password).collectLatest { registerResponse ->
+                _registerResult.value = registerResponse
+            }
+        }
+    }
 }
