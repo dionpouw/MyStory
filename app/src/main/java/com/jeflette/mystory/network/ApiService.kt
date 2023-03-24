d3d033230b0f48fe8547d56fd8c24a8bbb92c717
dionpouw <aldion.sumampouw@gmail.com>
2023-03-24 22:48:02 +0800

    Api Service

--- app/src/main/java/com/jeflette/mystory/network/ApiService.kt
+++ app/src/main/java/com/jeflette/mystory/network/ApiService.kt
@@ -1,7 +1,37 @@
 package com.jeflette.mystory.network
 
-interface ApiService {
+import com.jeflette.mystory.data.remote.response.CommonResponse
+import com.jeflette.mystory.data.remote.response.ListStoriesResponse
+import com.jeflette.mystory.data.remote.response.LoginResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.*
 
-    @Post
+interface ApiService {
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun submitRegister(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<CommonResponse>
+
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun submitLogin(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<LoginResponse>
+
+    @GET("stories")
+    suspend fun getStories(@Header("Authorization") token: String): Response<ListStoriesResponse>
+
+    @Multipart
+    @POST("stories")
+    suspend fun addStory(
+        @Header("Authorization") token: String,
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody
+    ): Response<CommonResponse>
 }
-// Authorization: Bearer <token>
